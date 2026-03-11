34351ad5e76db1c2573756f55f394ebdc5eb27fa
Kieran <kieran.macmanus.2023@uni.strath.ac.uk>
2026-03-11 15:22:56 +0000

    Stage 3 safe usernames feature, even if same time no 2 clients can register same name

--- src/Chatroom.java
+++ src/Chatroom.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Set;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,7 +13,7 @@ public class Chatroom {
     private static final Set<ClientHandling> clients =
             Collections.synchronizedSet(new HashSet<>());
 
-    private static final Map<String, ClientHandling> usernames = new HashMap<>();
+    private static final Map<String, ClientHandling> usernames = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws IOException {
         int port = 5000;
@@ -41,16 +42,7 @@ public class Chatroom {
     }
 
     public static boolean registerUsername(String username, ClientHandling client) {
-        if (usernames.containsKey(username)) {
-            return false;
-        }
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException ignored) {}
-
-        usernames.put(username, client);
-        return true;
+        return usernames.putIfAbsent(username, client) == null;
     }
 
     public static void clientRemove(ClientHandling client) {
