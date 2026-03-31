9c80c5b40f1e715c49b8007854b404fa5fe15ba7
owenmccutcheon <158854426+owenmccutcheon@users.noreply.github.com>
2026-03-31 17:16:20 +0100

    Updated TO FORCE FAILURE

--- PythonChat/python_chat.py
+++ PythonChat/python_chat.py
@@ -1,6 +1,6 @@
 from socket import *
-import threading
 import sys
+import time
 
 if len(sys.argv) != 3:
     print("Usage: python python_chat.py <host> <port>")
@@ -12,27 +12,11 @@ port = int(sys.argv[2])
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect((host, port))
 
-running = True
-
-def receive():
-    global running
-    try:
-        while running:
-            data = sock.recv(5000)
-            if not data:
-                break
-            print("Peer:", data.decode("utf-8"), end="")
-    except:
-        pass
-    finally:
-        running = False
-
-threading.Thread(target=receive, daemon=True).start()
-
-import time
-
+sock.sendall(("/name test\n").encode("utf-8"))
 
 for i in range(200):
     sock.sendall(("/join group1\n").encode("utf-8"))
+    sock.sendall((f"msg {i}\n").encode("utf-8"))
+    time.sleep(0.001)
 
 sock.close()
